43da4a26c7835423f7eacd9f96e29f564d568839
Saehanseul <saehanseul@gmail.com>
2023-07-12 16:23:49 +0900

    Feat: add athentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/screens/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -13,11 +15,22 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tiktok clone',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white,
         primaryColor: const Color(0xFFE9435A),
-        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
       ),
-      home: Container(),
+      home: const SignUpScreen(),
     );
   }
 }
